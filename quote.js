fd75aca78afa72cb5cc7eaaacd7bf10d13c150e8
Liam <31754174+liam@users.noreply.github.com>
2019-03-26 00:58:34 -0700

    Adding js quote array

---
+++ quote.js
@@ -0,0 +1,30 @@
+function callQuote(quoteNumber){
+	var quotes = [
+		"Our greatest glory is not in never falling, but in rising every time we fall.<br />- Confucius",
+		"All our dreams can come true, if we have the courage to pursue them.<br />- Walt Disney",
+		"It does not matter how slowly you go as long as you do not stop.<br />- Confucius",
+		"Everything you've ever wanted is on the other side of fear.<br />- George Addair",
+		"Success is not final, failure is not fatal: it is the courage to continue that counts.<br />- Winston Churchill",
+		"Hardships often prepare ordinary people for an extraordinary destiny.<br />- C.S. Lewis",
+		"I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear.<br />- Nelson Mandela",
+		"There is only one thing that makes a dream impossible to achieve: the fear of failure.<br />- Paulo Coelho",
+		"Your true success in life begins only when you make the commitment to become excellent at what you do.<br />- Brian Tracy",
+		"Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going.<br />- Chantal Sutherland",
+		"Definiteness of purpose is the starting point of all achievement.<br />- W. Clement Stone",
+		"If you believe it will work out, you'll see opportunities. If you believe it won't, you will see obstacles.<br />- Wayne Dyer",
+		"Permanence, perseverance and persistence in spite of all obstacles, discouragements, and impossibilities: It is this, that in all things distinguishes the strong soul from the weak.<br />- Thomas Carlyle",
+		"Success means doing the best we can with what we have. Success is the doing, not the getting; in the trying, not the triumph. Success is a personal standard, reaching for the highest that is in us, becoming all that we can be.<br />- Zig Ziglar",
+		"If you set goals and go after them with all the determination you can muster, your gifts will take you places that will amaze you.<br />- Les Brown",
+		"Believe you can and you're halfway there.<br />- Theodore Roosevelt",
+		"Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.<br />- Francis of Assisi",
+		"It is never too late to be what you might have been.<br />- George Eliot",
+		"Never limit yourself because of others' limited imagination; never limit others because of your own limited imagination.<br />- Mae Jemison",
+		"Each person must live their life as a model for others.<br />- Rosa Parks",
+		"No matter what people tell you, words and ideas can change the world.<br />- Robin Williams",
+		"If you look at what you have in life, you'll always have more.<br />- Oprah Winfrey",
+		"Let us make our future now, and let us make our dreams tomorrow's reality.<br />- Malala Yousafzai",
+		"You must do the things you think you cannot do.<br />- Eleanor Roosevelt",
+		"We must be willing to let go of the life we planned so as to have the life that is waiting for us.<br />- Joseph Campbell"
+	];
+	return quotes[quoteNumber];
+}
